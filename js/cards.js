314f8c6f45dd4619841553839d92beab3f0c9b14
Alina Flaynger <alinaflaynger@gmail.com>
2024-02-04 20:06:55 +0300

    init card css

--- js/cards.js
+++ js/cards.js
@@ -13,22 +13,54 @@ const WILDBERRIES_URL = fetch('https://65b5321841db5efd2867694f.mockapi.io/wildb
                     imageElement.src = item.img;
                     imageElement.alt = item.name;
 
-                    const priceElement = document.createElement('p');
+                    const priceElement = document.createElement('div');
                     priceElement.className = 'price';
                     priceElement.textContent = `$${item.price}`;
+                    const priceElementOld=document.createElement('span');
+                    priceElementOld.className='price__old'
+                    priceElementOld.textContent = '10$';
+                    const priceElementSale=document.createElement('span');
+                    priceElementSale.className='price__sale'
+                    priceElementSale.textContent = `$${item.price}`;
+                    const priceElementActual=document.createElement('span');
+                    priceElementActual.className='price__actual'
+                    
+                    const deliveryElement=document.createElement('h2')
+                    deliveryElement.className='delivery'
+                    deliveryElement.textContent='Завтра'
 
-                    const nameElement = document.createElement('h2');
+                    const nameElement = document.createElement('div');
                     nameElement.className = 'name';
-                    nameElement.textContent = item.name;
+                    const brendElement=document.createElement('span')
+                    brendElement.className="name__brend"
+                    brendElement.textContent = item.name;
+                    const brendinfoElement=document.createElement('span')
+                    brendinfoElement.className="brend__info"
+                    brendinfoElement.textContent = item.name;
+
+                    const ratinginfoElement=document.createElement('div')
+                    ratinginfoElement.className='ratinginfo'
+                    // const rating=document.createElement('span')
+                    // rating.className='rating'
+                    // rating.innerText= '5';
+                    const rewiew=document.querySelector('.rewiew')
+                    const rating=document.querySelector('.rating')
+                    
+
+                
 
                     const addToCartButton = document.createElement('button');
                     addToCartButton.className = 'add-to-cart-button';
-                    addToCartButton.textContent = 'Добавить в корзину';
+                    addToCartButton.textContent = 'В корзину';
                     addToCartButton.addEventListener('click', () => addToCart(item.id));
 
                     card.appendChild(imageElement);
                     card.appendChild(priceElement);
+                    priceElement.append (priceElementActual, priceElementSale,priceElementOld)
                     card.appendChild(nameElement);
+                    nameElement.append(brendElement, brendinfoElement)
+                    card.appendChild(ratinginfoElement);
+                    ratinginfoElement.append(rating,rewiew)
                     card.appendChild(addToCartButton);
 
                     cardsContainer.appendChild(card);
