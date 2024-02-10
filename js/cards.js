6645d7b14ca13743aa99f609975d0af349f2bc6e
Pavel Seduan <sphreofdie@gmail.com>
2024-02-10 06:30:45 +0300

    v 1.0.7

--- js/cards.js
+++ js/cards.js
@@ -5,64 +5,12 @@ WILDBERRIES_URL
     .then(data => {
         const cardsContainer = document.querySelector('.cards__container');
 
-        data.forEach(item => {
-            const card = document.createElement('div');
-            card.className = 'card';
+        // Создание и перемешивание массива карточек
+        const shuffledData = shuffleArray(data);
 
-            const imageElement = document.createElement('img');
-            imageElement.src = item.img;
-            imageElement.alt = item.name;
-
-            const priceElement = document.createElement('div');
-            priceElement.className = 'price';
-
-            const priceElementOld = document.createElement('span');
-            priceElementOld.className = 'price__old';
-            const oldPrice = parseFloat(item.price) + (parseFloat(item.price) * 0.15);
-            priceElementOld.textContent = formatPrice(oldPrice);
-
-            const priceElementSale = document.createElement('span');
-            priceElementSale.className = 'price__sale';
-            priceElementSale.textContent = formatPrice(item.price);
-
-            const deliveryElement = document.createElement('h2');
-            deliveryElement.className = 'delivery';
-            deliveryElement.textContent = 'Завтра';
-
-            const nameElement = document.createElement('div');
-            nameElement.className = 'name';
-
-            const brendElement = document.createElement('span');
-            brendElement.className = 'name__brend';
-            brendElement.textContent = item.name;
-
-            const ratinginfoElement = document.createElement('div');
-            ratinginfoElement.className = 'rating';
-
-            const rewiew = document.createElement('span');
-            rewiew.className = 'rewiew';
-            ratinginfoElement.textContent = item.rating;
-
-            const addToCartButton = document.createElement('button');
-            addToCartButton.className = 'add-to-cart-button';
-            addToCartButton.textContent = 'В корзину';
-            addToCartButton.addEventListener('click', () => addToCart(item.id));
-
-            card.id = item.id;
-            card.name = item.name;
-            card.oldPrice = oldPrice.toFixed(2);
-            card.salePrice = item.price;
-            card.rating = item.rating;
-
-            card.appendChild(imageElement);
-            priceElement.appendChild(priceElementSale);
-            priceElement.appendChild(priceElementOld);
-            card.appendChild(priceElement);
-            card.appendChild(nameElement);
-            nameElement.appendChild(brendElement);
-            card.appendChild(ratinginfoElement);
-            card.appendChild(addToCartButton);
-
+        // Отображение карточек на странице
+        shuffledData.forEach(item => {
+            const card = createCardElement(item);
             cardsContainer.appendChild(card);
         });
     })
@@ -82,4 +30,76 @@ function formatPrice(price) {
     } else {
         return `${rubles},${kop}р.`;
     }
+}
+
+// Функция для перемешивания массива
+function shuffleArray(array) {
+    const shuffledArray = [...array];
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+    }
+    return shuffledArray;
+}
+
+// Функция для создания элемента карточки
+function createCardElement(item) {
+    const card = document.createElement('div');
+    card.className = 'card';
+
+    const imageElement = document.createElement('img');
+    imageElement.src = item.img;
+    imageElement.alt = item.name;
+
+    const priceElement = document.createElement('div');
+    priceElement.className = 'price';
+
+    const priceElementOld = document.createElement('span');
+    priceElementOld.className = 'price__old';
+    const oldPrice = parseFloat(item.price) + (parseFloat(item.price) * 0.15);
+    priceElementOld.textContent = formatPrice(oldPrice);
+
+    const priceElementSale = document.createElement('span');
+    priceElementSale.className = 'price__sale';
+    priceElementSale.textContent = formatPrice(item.price);
+
+    const deliveryElement = document.createElement('h2');
+    deliveryElement.className = 'delivery';
+    deliveryElement.textContent = 'Завтра';
+
+    const nameElement = document.createElement('div');
+    nameElement.className = 'name';
+
+    const brendElement = document.createElement('span');
+    brendElement.className = 'name__brend';
+    brendElement.textContent = item.name;
+
+    const ratinginfoElement = document.createElement('div');
+    ratinginfoElement.className = 'rating';
+
+    const rewiew = document.createElement('span');
+    rewiew.className = 'rewiew';
+    ratinginfoElement.textContent = item.rating;
+
+    const addToCartButton = document.createElement('button');
+    addToCartButton.className = 'add-to-cart-button';
+    addToCartButton.textContent = 'В корзину';
+    addToCartButton.addEventListener('click', () => addToCart(item.id));
+
+    card.id = item.id;
+    card.name = item.name;
+    card.oldPrice = oldPrice.toFixed(2);
+    card.salePrice = item.price;
+    card.rating = item.rating;
+
+    card.appendChild(imageElement);
+    priceElement.appendChild(priceElementSale);
+    priceElement.appendChild(priceElementOld);
+    card.appendChild(priceElement);
+    card.appendChild(nameElement);
+    nameElement.appendChild(brendElement);
+    card.appendChild(ratinginfoElement);
+    card.appendChild(addToCartButton);
+
+    return card;
 }
--- js/search.js
+++ js/search.js
@@ -1,9 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     const searchForm = document.getElementById('searchForm');
     const searchInput = document.getElementById('searchInput');
-    let noResultsMessage; // Переменная для хранения элемента с сообщением о том, что ничего не найдено
+    let noResultsMessage;
 
-    // Функция фильтрации карточек
     function filterCards(searchTerm, cards) {
         cards.forEach(card => {
             const cardName = card.querySelector('.name__brend').textContent.toLowerCase();
@@ -17,28 +16,26 @@ document.addEventListener('DOMContentLoaded', () => {
         return matchingCards;
     }
 
-    // Функция для показа всех карточек
     function showAllCards(cards) {
         cards.forEach(card => {
             card.style.display = 'block';
         });
     }
 
-    // Обработчик события отправки формы
     searchForm.addEventListener('submit', (event) => {
         event.preventDefault();
         const cards = document.querySelectorAll('.card');
         const searchTerm = searchInput.value.trim().toLowerCase();
         if (searchTerm === '') {
-            showAllCards(cards); // Если строка поиска пуста, показываем все карточки
-            removeNoResultsMessage(); // Удаляем сообщение о том, что ничего не найдено, если оно было показано
+            showAllCards(cards);
+            removeNoResultsMessage(); 
         } else {
             const matchingCards = filterCards(searchTerm, cards);
             if (matchingCards.length === 0) {
-                removeNoResultsMessage(); // Удаляем старое сообщение, если оно существует
-                showNoResultsMessage(); // Показываем новое сообщение о том, что ничего не найдено
+                removeNoResultsMessage();
+                showNoResultsMessage(); 
             } else {
-                removeNoResultsMessage(); // Удаляем сообщение, если есть совпадения
+                removeNoResultsMessage();
             }
         }
     });
@@ -48,12 +45,11 @@ document.addEventListener('DOMContentLoaded', () => {
         const cards = document.querySelectorAll('.card');
         const searchTerm = searchInput.value.trim();
         if (searchTerm === '') {
-            showAllCards(cards); // Если строка поиска пуста, показываем все карточки
-            removeNoResultsMessage(); // Удаляем сообщение о том, что ничего не найдено, если оно было показано
+            showAllCards(cards);
+            removeNoResultsMessage()
         }
-    }, 1000); // Проверяем каждую секунду
+    }, 1000);
 
-    // Функция для показа сообщения о том, что ничего не найдено
     function showNoResultsMessage() {
         noResultsMessage = document.createElement('div');
         noResultsMessage.textContent = 'Ничего не найдено по запросу';
@@ -61,7 +57,6 @@ document.addEventListener('DOMContentLoaded', () => {
         document.querySelector('.cards__container').appendChild(noResultsMessage);
     }
 
-    // Функция для удаления сообщения о том, что ничего не найдено
     function removeNoResultsMessage() {
         if (noResultsMessage) {
             noResultsMessage.remove();
